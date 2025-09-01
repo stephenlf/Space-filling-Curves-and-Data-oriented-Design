64b389fa3c7db1a42cda6aefd01794c466c6bf08
stephenlf <stephenlfunk@gmail.com>
2025-09-01 13:42:05 -0400

    Ran benchmarks. The hilbert curve is slower by a factor of ~50x, presumably because of all the computation is requires to simply calculate the index to use. We could try caching the index, but then we might as well store the value in the cache.
    I wonder if there are better ways to calculate indices while preserving cache locality? These look promising:
    - [A Novel Hilbert Curve for Cache-locality Preserving Loops](http://eprints-dev5.cs.univie.ac.at/5726/1/loops.pdf)
    - [Efficient Computation of a Hilbert Curve](https://github.com/becheran/fast-hilbert)
    
    Also, the flat array is slightly slow than the array of arrays. Is that because of compiler optimization? Maybe it's because everything is on the stack? Maybe that liiiiitle bit of computation to flatten a coordinate pair is enough to lose all gains.

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -1,24 +1,23 @@
-
-#![allow(unused)]
-fn main() {
 use std::hint::black_box;
 use criterion::{criterion_group, criterion_main, Criterion};
 use cache_locality_experiment::{game, grid};
 
 pub fn criterion_benchmark(c: &mut Criterion) {
+  let mut group = c.benchmark_group("cache-locality-experiment");
+  group.sample_size(50);
+
   let mut vec_of_vecs = game::Game::new(grid::Grid::vec_of_vecs());
   vec_of_vecs.add_glider();
-  c.bench_function("vec_of_vecs 1000", |b| b.iter(|| vec_of_vecs.simulate(black_box(1000))));
+  group.bench_function("vec_of_vecs 1000", |b| b.iter(|| vec_of_vecs.simulate(black_box(100))));
 
   let mut flat_vec = game::Game::new(grid::Grid::flat_vec());
   flat_vec.add_glider();
-  c.bench_function("flat_vec 1000", |b| b.iter(|| flat_vec.simulate(black_box(1000))));
+  group.bench_function("flat_vec 1000", |b| b.iter(|| flat_vec.simulate(black_box(100))));
 
   let mut hilbert = game::Game::new(grid::Grid::hilbert());
   hilbert.add_glider();
-  c.bench_function("hilbert 1000", |b| b.iter(|| hilbert.simulate(black_box(1000))));
+  group.bench_function("hilbert 1000", |b| b.iter(|| hilbert.simulate(black_box(100))));
 }
 
 criterion_group!(benches, criterion_benchmark);
 criterion_main!(benches);
-}
